9dbc3260c57a18fe1511ebf49b54899aa7a4f136
HEFLoRa <david.gackstetter@tum.de>
2020-12-22 10:02:40 +0100

    feature/implements logic to retrieve outputs from ttn_thing_registration

---
+++ configs/scenarios/get_input.py
@@ -0,0 +1,8 @@
+import shutil
+
+# Gets inputs from local ttn_thing_registration component for testing purposes
+
+original = '/home/konrad/Desktop/HEFLoRa/Components/TTN_thing_registration/output/device_registration_parameters_ttn.txt'
+target = '/home/konrad/Desktop/HEFLoRa/Components/TTN_database_interface/configs/scenarios/input.txt'
+
+shutil.copyfile(original, target)
