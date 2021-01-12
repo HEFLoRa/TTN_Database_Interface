ce483cca05ab097ba4afd590bb151432de516af7
kjbaumann <baumannkonrad@outlook.de>
2021-01-12 20:04:35 +0100

    feat/implements new database and id generator

--- create_new_entry.py
+++ create_new_entry.py
@@ -1,31 +1,28 @@
 import json
 import os
 from configs.database_connection import cur, conn
-from unique_thing_id.id_generator import id
+
 
 # Calls get_input to get data from ttn_thing_registration
-os.system('python3 configs/scenarios/get_input.py')
+# os.system('python3 configs/scenarios/get_input.py')
 
 # Gets input and stores it in variable
 input = open("configs/scenarios/input_ttn.json")
 input_data = json.load(input)
 
 # Writes input in thing table
-cur.execute("INSERT INTO thing VALUES(%s, 'fake_user_%s', 'fake_client', 00, null, null, true);" % (str(id), str(id)))
+cur.execute("INSERT INTO thing VALUES(default, 'fake_user', 'fake_client', 01, null, null, true);")
+
+# Pulls unique_thing_id from thing table
+cur.execute("SELECT unique_thing_id FROM thing ORDER BY unique_thing_id DESC LIMIT 1")
+id = cur.fetchall()
+print(id[0][0])
 
 # TODO check for ttn -> source_ttn / source_third_party
 
 # Writes input in source_ttn table
-cur.execute("INSERT INTO source_ttn VALUES(%s, '%s', '%s', '%s', 'fake_address', null);" % (str(id), input_data["Application ID"], input_data["Access Key"], input_data["Device ID"]))
+cur.execute("INSERT INTO source_ttn VALUES(%s, '%s', '%s', '%s', 'fake_address', null);" % (str(id[0][0]), input_data["Application ID"], input_data["Access Key"], input_data["Device ID"]))
 
 conn.commit()
-
-cur.execute("SELECT * FROM source_ttn")
-result = cur.fetchall()
-
 cur.close()
 conn.close()
-
-print(result)
-
-
