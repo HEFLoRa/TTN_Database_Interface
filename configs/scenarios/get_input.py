b406ed3dbc389e33b63f5097e240b122c5db9d12
kjbaumann <baumannkonrad@outlook.de>
2021-01-29 10:21:59 +0100

    structure/changes input options

--- configs/scenarios/get_input.py
+++ configs/scenarios/get_input.py
@@ -1,13 +1,13 @@
 import shutil
 
 # Gets inputs from local ttn_thing_registration component for testing purposes
-original = '/home/konradb/Desktop/HEF/Components/Backend/TTN_thing_registration/output/device_registration_parameters_ttn.txt'
-target = '/home/konradb/Desktop/HEF/Components/Backend/TTN_Database_Interface/configs/scenarios/input_ttn.json'
+original = '/home/konradb/Desktop/HEF/Workshops/5_Workshop/Demo/TTN_thing_registration-main/output/device_registration_parameters_ttn.txt'
+target = '/home/konradb/Desktop/HEF/Workshops/5_Workshop/Demo/TTN_Database_Interface/configs/scenarios/input_ttn.json'
 shutil.copyfile(original, target)
 
 # Gets input from local frost_cli component for testing purposes
-original = '/home/konradb/Desktop/HEF/Components/Backend/FROST_CLI/output/device_registration_parameters_frost.txt'
-target = '/home/konradb/Desktop/HEF/Components/Backend/TTN_Database_Interface/configs/scenarios/input_frost.json'
+original = '/home/konradb/Desktop/HEF/Workshops/5_Workshop/Demo/FROST_CLI-main/output/device_registration_parameters_frost.txt'
+target = '/home/konradb/Desktop/HEF/Workshops/5_Workshop/Demo/TTN_Database_Interface/configs/scenarios/input_frost.json'
 shutil.copyfile(original, target)
 
 
--- create_new_entry.py
+++ create_new_entry.py
@@ -3,15 +3,17 @@ import os
 from configs.database_connection import cur, conn
 
 
-# Calls get_input to get data from ttn_thing_registration
+# Calls get_input to get data from ttn_thing_registration and frost_cli
 os.system('python3 configs/scenarios/get_input.py')
 
 # Gets input and stores it in variable
 input = open("configs/scenarios/input_ttn.json")
-input_data = json.load(input)
+input_data_ttn = json.load(input)
+input = open("configs/scenarios/input_frost.json")
+input_data_frost = json.load(input)
 
 # Writes input in thing table
-cur.execute("INSERT INTO thing VALUES(default, 'fake_user', 'fake_client', 01, null, null, true);")
+cur.execute("INSERT INTO thing VALUES(default, 'fake_user', 'fake_client', %s, null, null, true);" % (input_data_frost["Things"][0])) 
 
 # Pulls unique_thing_id from thing table
 cur.execute("SELECT unique_thing_id FROM thing ORDER BY unique_thing_id DESC LIMIT 1")
@@ -20,7 +22,7 @@ id = cur.fetchall()
 # TODO check for ttn -> source_ttn / source_third_party
 
 # Writes input in source_ttn table
-cur.execute("INSERT INTO source_ttn VALUES(%s, '%s', '%s', '%s', 'localhost:8080', null, '%s', '%s', 'localhost');" % (str(id[0][0]), input_data["Application ID"], input_data["Device ID"], input_data["Access Key"], input_data["DevEUI"], input_data["AppEUI"]))
+cur.execute("INSERT INTO source_ttn VALUES(%s, '%s', '%s', '%s', 'localhost:8080', null, '%s', '%s', 'localhost');" % (str(id[0][0]), input_data_ttn["Application ID"], input_data_ttn["Device ID"], input_data_ttn["Access Key"], input_data_ttn["DevEUI"], input_data_ttn["AppEUI"]))
 
 conn.commit()
 cur.close()
